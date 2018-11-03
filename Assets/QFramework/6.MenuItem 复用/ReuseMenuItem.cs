d7f950ddda31a47624d452b999e6065f362d4a18
liangxieq <liangxiegame@163.com>
2018-11-03 17:33:07 +0800

    0.0.1.106 MenuItem 复用

---
+++ Assets/QFramework/6.MenuItem 复用/ReuseMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public class ReuseMenuItem : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/6.MenuItem 复用")]
+		private static void MenuClicked()
+		{
+			EditorApplication.ExecuteMenuItem("QFramework/4.导出 UnityPackage");
+			Application.OpenURL("file:///" + Path.Combine(Application.dataPath, "../"));
+		}
+#endif
+	}
+}
