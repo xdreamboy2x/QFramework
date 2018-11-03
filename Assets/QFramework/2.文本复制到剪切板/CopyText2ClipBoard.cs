f04baac5e6d1b59c045d57494ad8727497d2b3a9
liangxieq <liangxiegame@163.com>
2018-11-03 14:48:52 +0800

    v0.0.1.102 文本复制到剪切板

---
+++ Assets/QFramework/2.文本复制到剪切板/CopyText2ClipBoard.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public static class CopyText2Clipboard
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/2.复制文本到剪切板")]
+#endif
+		private static void CopyText()
+		{
+			GUIUtility.systemCopyBuffer = "要复制的关键字";
+		}
+	}
+}
