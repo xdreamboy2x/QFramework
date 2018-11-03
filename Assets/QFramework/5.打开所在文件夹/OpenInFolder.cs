880acc53abd3ea090e549e0d6ed32ca61998137e
liangxieq <liangxiegame@163.com>
2018-11-03 16:04:18 +0800

    v0.0.1.105

---
+++ Assets/QFramework/5.打开所在文件夹/OpenInFolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public class OpenInFolder
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/5.打开所在文件夹")]
+		private static void MenuClicked()
+		{
+			Application.OpenURL("file:///" +  Application.dataPath);
+		}
+#endif
+	}
+}
