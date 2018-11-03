5e74418c4c8d87c307a99a888e61831ec68e8b5e
liangxieq <liangxiegame@163.com>
2018-11-03 17:33:46 +0800

    v0.0.1.107 自定义快捷键

---
+++ Assets/QFramework/7.自定义快捷键/CustomShortCut.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public class CustomShortCut : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/7.自定义快捷键 %e")]
+		private static void MenuClicked()
+		{
+			EditorApplication.ExecuteMenuItem("QFramework/6.MenuItem 复用");
+		}
+#endif
+	}
+}
