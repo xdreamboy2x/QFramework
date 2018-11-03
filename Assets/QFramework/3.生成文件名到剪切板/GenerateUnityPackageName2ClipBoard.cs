8c2b13357435b0fa5e1f07041730df24360578a9
liangxieq <liangxiegame@163.com>
2018-11-03 15:12:57 +0800

    v0.0.1.103 生成文件名到剪切板

---
+++ Assets/QFramework/3.生成文件名到剪切板/GenerateUnityPackageName2ClipBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public class GenerateUnityPackageName2ClipBoard 
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/3.生成文件名到剪切板")]
+#endif
+		private static void MenuClicked()
+		{
+			GUIUtility.systemCopyBuffer = "QFramework_" + DateTime.Now.ToString("yyyyMMdd_hh");		
+		}
+	}
+}
