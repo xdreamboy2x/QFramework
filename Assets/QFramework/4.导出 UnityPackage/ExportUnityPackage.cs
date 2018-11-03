685555b0738a80adcb8e89b319d5c70f2889193f
liangxieq <liangxiegame@163.com>
2018-11-03 15:45:07 +0800

    v0.0.1.104 导出 UnityPackage

---
+++ Assets/QFramework/4.导出 UnityPackage/ExportUnityPackage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace QFramework
+{
+	public class ExportUnityPackage : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		[MenuItem("QFramework/4.导出 UnityPackage")]
+		private static void MenuClicked()
+		{
+			var assetPathName = "Assets/QFramework";
+			var fileName = "QFramework_" + DateTime.Now.ToString("yyyyMMdd_hh") + ".unitypackage";
+			AssetDatabase.ExportPackage(assetPathName, fileName, ExportPackageOptions.Recurse);
+		}
+#endif
+	}
+}
