9a5c746c3666030bd5ce63e709c44c45ca120f72
liangxieq <liangxiegame@163.com>
2018-11-03 13:59:06 +0800

    v0.0.1.101 文件名生成

---
+++ Assets/QFramework/1.文件名生成/LogFileName.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using System;
+
+namespace QFramework
+{
+	public static class LogFileName 
+	{
+		#if UNITY_EDITOR
+		[MenuItem("QFramework/1.生成 unitypackage 名字")]
+		#endif
+		private static void GenerateUnityPackageName()
+		{
+			Debug.Log("QFramework_" + DateTime.Now.ToString("yyyyMMdd_hh"));
+		}
+	}
+}
